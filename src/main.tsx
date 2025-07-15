e3755a73117de1adf7e02679728e81921e367659
Divyank Dhadi <divyank004@gmail.com>
2025-07-15 14:46:28 +0200

    add Authentication gaurd to components

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,24 @@ import App from './App.js'
 import { BrowserRouter, Routes, Route } from "react-router";
 import URLDetailPage from './pages/URLDetailPage';
 import Dashboard from './pages/Dashboard.js';
+import { Navigate } from "react-router";
+
+const isUserAuthenticated = () => {
+  const token = localStorage.getItem('authToken');
+  // TODO check for expired token as well
+  if(token === null || token === undefined) {
+    return false;
+  }
+  return true;
+}
+const ProtectedRoute = ({ children }) => {
+  const userAuthorized = isUserAuthenticated();
+
+  if (!userAuthorized) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,8 +29,8 @@ createRoot(document.getElementById('root')!).render(
     <Routes>
       <Route path="/">
         <Route index element={<App/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
-        <Route path="/url/:id" element={<URLDetailPage/>} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard/></ProtectedRoute>} />
+        <Route path="/url/:id" element={<ProtectedRoute><URLDetailPage/></ProtectedRoute>} />
       </Route>
     </Routes>
   </BrowserRouter>
