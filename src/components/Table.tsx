a2464c902399b422192fd3288abc3f5cb9d8295d
Divyank Dhadi <divyank004@gmail.com>
2025-07-16 15:56:03 +0200

    make the Table view look modern

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -78,16 +78,16 @@ const Table = <T1, T2 = undefined>({
   };
 
   return (
-    <div className="w-full">
-      <div className="h-80 sm:h-100 md:h-120 lg:h-140 max-h-full md:max-h-screen overflow-y-auto">
-        <table>
-          <thead className="bg-gray-50 sticky top-0 z-10">
+    <div className="w-full bg-white/10 backdrop-blur-lg">
+      <div className="bg-white/10 h-80 sm:h-100 md:h-120 lg:h-140 max-h-full md:max-h-screen overflow-y-auto">
+        <table className="min-w-full ">
+          <thead className="bg-white/2 sticky top-0 z-10">
             <tr>
               {showCheckbox && (
                 <th className="px-2 py-1.5 sm:px-6 sm:py-3 text-left">
                   <button
                     onClick={selectAllRows}
-                    className="text-gray-400 hover:text-gray-600"
+                    className="text-white hover:text-gray-200"
                   >
                     {selectedRows.size === rows.length && rows.length > 0 ? (
                       <SquareCheckBig className="h-5 w-5" />
@@ -100,28 +100,28 @@ const Table = <T1, T2 = undefined>({
               {columns.map((item) => (
                 <th
                   key={item.name}
-                  className="px-2 py-1.5 sm:px-6 sm:py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  className="px-2 py-1.5 sm:px-6 sm:py-3 text-left text-xs font-medium text-white uppercase tracking-wider"
                 >
                   {item.label}
                 </th>
               ))}
             </tr>
           </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
+          <tbody className="bg-white/10 divide-y divide-gray-200">
             {paginatedRows.map((item) => (
               <tr
                 key={item[unqieKeyInRows]}
-                className="hover:bg-gray-50"
+                className="cursor-pointer"
                 onClick={() => rowClicked && rowClicked(item[unqieKeyInRows])}
               >
                 {showCheckbox && (
-                  <td className="px-2 py-1 sm:px-6 sm:py-3  whitespace-nowrap">
+                  <td className=" px-2 py-1 sm:px-6 sm:py-3  whitespace-nowrap">
                     <button
                       onClick={(e) => {
                         e.stopPropagation();
                         rowSelected(item[unqieKeyInRows]);
                       }}
-                      className="text-gray-400 hover:text-gray-600"
+                      className="text-white hover:text-gray-200"
                     >
                       {selectedRows.has(item[unqieKeyInRows]) ? (
                         <SquareCheckBig className="h-5 w-5" />
@@ -154,7 +154,7 @@ const Table = <T1, T2 = undefined>({
       </div>
       {/* Footer */}
       {!isObjNullOrUndefined(pagination) && !isObjNullOrUndefined(footer) && (
-        <div className="px-6 py-4 bg-white border-t border-gray-200 shadow-sm">
+        <div className="px-6 py-4 bg-white border-t border-gray-200 shadow-sm bg-white/5 backdrop-blur-lg">
           <div className="flex flex-col sm:flex-row justify-center sm:items-center sm:justify-between gap-4 ">
             {!isObjNullOrUndefined(pagination) && (
               <div className="flex items-center space-x-5">
@@ -163,45 +163,37 @@ const Table = <T1, T2 = undefined>({
                   <button
                     disabled={pageIndex === 0}
                     onClick={() => setPageIndex((prev) => prev - 1)}
-                    className="inline-flex items-center px-1 sm:px-3 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50  focus:ring-2 focus:ring-blue-500  disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-white transition-colors duration-200"
+                    className="inline-flex items-center px-1 sm:px-3 py-2 text-sm font-medium bg-white/10 border-white/20 rounded-md hover:bg-white/30 focus:ring-2 focus:ring-purple-500"
                   >
-                    <ChevronLeft className="h-4 w-4 mr-2" />
-                    <span className="hidden sm:block">Previous</span>
+                    <ChevronLeft className="h-4 w-4 mr-2" color="white" />
+                    <span className="hidden sm:block text-white">Previous</span>
                   </button>
 
                   <button
                     disabled={pageIndex >= totalPages - 1}
                     onClick={() => setPageIndex((prev) => prev + 1)}
-                    className="inline-flex items-center px-1 sm:px-3 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50 focus:ring-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-white transition-colors duration-200"
+                    className="inline-flex items-center px-1 sm:px-3 py-2 text-sm font-medium bg-white/10 border-white/20 rounded-md hover:bg-white/30 focus:ring-2 focus:ring-purple-500"
                   >
-                    <span className="hidden sm:block">Next</span>
-                    <ChevronRight className="h-4 w-4 mr-2" />
+                    <span className="hidden sm:block text-white">Next</span>
+                    <ChevronRight className="h-4 w-4 mr-2" color="white" />
                   </button>
                 </div>
-                <div className="text-sm text-gray-600 font-medium">
-                  Page{" "}
-                  <span className="font-semibold text-gray-900">
-                    {pageIndex + 1}
-                  </span>{" "}
-                  /{" "}
-                  <span className="font-semibold text-gray-900">
-                    {totalPages}
-                  </span>
+                <div className="text-sm text-gray-300 font-medium">
+                  Page <span className="font-semibold ">{pageIndex + 1}</span> /{" "}
+                  <span className="font-semibold">{totalPages}</span>
                 </div>
                 {/* Page Info */}
-                <div className="flex items-center space-x-2">
+                <div className="flex items-center space-x-2 text-gray-300">
                   {/* Rows per page selector */}
                   <div className="flex items-center space-x-2">
-                    <label className="text-sm text-gray-600 font-medium">
-                      Show:
-                    </label>
+                    <label className="text-sm font-medium">Show:</label>
                     <select
                       value={pageSize}
                       onChange={(e) => {
                         setPageSize(Number(e.target.value));
                         setPageIndex(0);
                       }}
-                      className="border border-gray-300 rounded-md px-3 py-1.5 text-sm bg-white focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 transition-colors duration-500"
+                      className="bg-white/10 border border-white/20  text-white rounded-md px-3 py-1.5 text-sm focus:outline-none focus:ring-2 focus:ring-purple-500"
                     >
                       {pagination?.pageSizeOptions.map((size) => (
                         <option key={size} value={size}>
@@ -218,7 +210,7 @@ const Table = <T1, T2 = undefined>({
             {!isObjNullOrUndefined(footer) && (
               <button
                 onClick={() => footer?.buttonOne.onAddUrl()}
-                className={`text-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors duration-200 ${footer?.buttonOne.className}`}
+                className={`text-center px-4 py-2 text-sm font-medium bg-white/10 border border-white/20  text-white rounded-md shadow-sm hover:bg-white/30  focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 ${footer?.buttonOne.className}`}
               >
                 {footer?.buttonOne.title}
               </button>
